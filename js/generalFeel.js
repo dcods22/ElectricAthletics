96ebfb5437416cc582ee67dfe6ea9b7ac2403563
Dcods22 <Daniel.cody2@marist.edu>
2014-09-08 18:09:57 -0400

    Added features to desktop style sheet

--- js/generalFeel.js
+++ js/generalFeel.js
@@ -3,3 +3,7 @@ $('textarea').autogrow();
 $(function() {
     $( document ).tooltip();
 });
+
+$('.navbar-nav a').on('click', function(){
+    $('.navbar-collapse').removeClass('collapse in').addClass('collapsing').removeClass('collapsing').addClass('collapse');
+});
